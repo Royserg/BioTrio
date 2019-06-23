95a0bfe9ab188eb108cf4ff6962fad39789696d4
plamistef <plamstef@abv.bg>
2019-06-23 19:21:33 +0200

    render empty table

--- src/main/resources/static/js/screening/prepareEvents.js
+++ src/main/resources/static/js/screening/prepareEvents.js
@@ -1,6 +1,6 @@
 function createEvents(selectedScreenings,openHour,closeHour) {
 
-    // console.log(selectedScreenings);
+    console.log(selectedScreenings);
     // console.log(typeof selectedScreenings);
     let timetable = new Timetable();
     timetable.addLocations([ 'loc']);
@@ -10,31 +10,36 @@ function createEvents(selectedScreenings,openHour,closeHour) {
     const API_KEY = '02325bf00c28d42c083b25b3be60b75e';
     const API_URL = 'https://api.themoviedb.org/3';
 
+    if(selectedScreenings.length > 0) {
+
     selectedScreenings.forEach(screening => {
 
-    $.ajax(`${API_URL}/movie/${screening.movieId}?api_key=${API_KEY}`)
-        .done(response => {
+        $.ajax(`${API_URL}/movie/${screening.movieId}?api_key=${API_KEY}`)
+            .done(response => {
 
-            let scheduleEvent = {
-                // "movieId": screening.movieId,
-                "title": response['original_title'],
-                "start":screening.time,
-                "end":calculateEndTime(screening.time,response['runtime']),
-            }
+                let scheduleEvent = {
+                    // "movieId": screening.movieId,
+                    "title": response['original_title'],
+                    "start":screening.time,
+                    "end":calculateEndTime(screening.time,response['runtime']),
+                }
 
-            let startHour = parseInt(scheduleEvent.start.slice(0,2));
-            let startMin = parseInt(scheduleEvent.start.slice(3,5));
+                let startHour = parseInt(scheduleEvent.start.slice(0,2));
+                let startMin = parseInt(scheduleEvent.start.slice(3,5));
 
-            let endHour = parseInt(scheduleEvent.end.slice(0,2));
-            let endMin = parseInt(scheduleEvent.end.slice(3,5));
+                let endHour = parseInt(scheduleEvent.end.slice(0,2));
+                let endMin = parseInt(scheduleEvent.end.slice(3,5));
 
-            timetable.addEvent(scheduleEvent.title,'loc' , new Date(2000,1,1,startHour,startMin),
-                                                              new Date(2000,1,1,endHour,endMin));
+                timetable.addEvent(scheduleEvent.title,'loc' , new Date(2000,1,1,startHour,startMin),
+                                                                  new Date(2000,1,1,endHour,endMin));
+                renderTable(timetable);
 
-            renderTable(timetable);
-        });
-    })
-
+            });
+        })
+    }
+    else {
+        renderTable(timetable);
+    }
     return timetable;
 }
 
