3d291d9997d327ee9340e299161f9a2f449553e2
Issa <issa0047@stud.kea.dk>
2019-12-12 12:00:54 +0100

    fdsbl

--- src/main/java/com/biotrio/nocristina/NocristinaApplication.java
+++ src/main/java/com/biotrio/nocristina/NocristinaApplication.java
@@ -2,12 +2,19 @@ package com.biotrio.nocristina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NocristinaApplication {
+public class NocristinaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NocristinaApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NocristinaApplication.class);
+
+    }
 }
